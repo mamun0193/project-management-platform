3cf385758f98200298bf55006eba6bdc869bf4a1
mamun0193 <mamunrahaman019@gmail.com>
2026-02-26 23:53:40 +0530

    refactor: restructure project with src directory and add cors support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import exprss from "express";
+import app from "./app.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const app = exprss();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
