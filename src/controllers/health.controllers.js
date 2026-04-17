a66e10608f474424ddf577566b0a54ea5fff9ae4
mamun0193 <mamunrahaman019@gmail.com>
2026-04-18 00:36:51 +0530

    feat: Add async Handler

--- src/controllers/health.controllers.js
+++ src/controllers/health.controllers.js
@@ -1,6 +1,7 @@
 import {apiResponse} from "../utils/apiResponse.js"
-
-const healthCheck = (req, res) => {
+import { asyncHandler } from "../utils/async-handler.js";
+/*
+const healthCheck = async (req, res) => {
   try {
     res
       .status(200)
@@ -9,4 +10,12 @@ const healthCheck = (req, res) => {
     res.status(500).json(apiResponse(false, "Internal server error", null));
   }
 };
+*/
+
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new apiResponse(200, { message: "Server is healthy" }));
+});
+
 export { healthCheck };
